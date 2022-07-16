3bbf34512e8aef44948e2e6ebbf0a6c1bf367933
kleyson Castro <kleysoncastro99@gmail.com>
2022-07-16 20:19:38 -0300

    :fire: firt commit 	compilando com class de acesse spi

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "../component/spi/include/spi.hpp"
+
+extern "C" void app_main();
+
+void app_main(void)
+{   
+
+ESP_LOGI("main", "main inicianda");
+
+}// end app_main
+
